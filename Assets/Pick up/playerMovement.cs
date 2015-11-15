ef8c5db5a3bd16cc0040ed2422d23290fe544b97
Nexus64 <pedro9romero4gonzalez@gmail.com>
2015-11-15 21:09:29 +0100

    Basic character movement

---
+++ Assets/Pick up/playerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.RightArrow)){
+			transform.Translate(Vector2.right*speed);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			transform.Translate(-Vector2.right*speed);
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow)){
+			transform.Translate(Vector2.up*speed);
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow)){
+			transform.Translate(-Vector2.up*speed);
+		}
+	}
+}
