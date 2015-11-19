df91a61c59bc4f552fe789f81a27085ba679a63c
Nexus64 <pedro9romero4gonzalez@gmail.com>
2015-11-19 10:55:32 +0100

    Improve movement + Borders

--- Assets/Pick up/playerMovement.cs
+++ Assets/Pick up/playerMovement.cs
@@ -4,27 +4,34 @@ using System.Collections;
 public class playerMovement : MonoBehaviour {
 
 	public float speed;
+	public float s =0;
+	private Rigidbody2D rigidBody2D;
 	// Use this for initialization
 	void Start () {
-	
+		rigidBody2D = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.RightArrow)){
-			transform.Translate(Vector2.right*speed);
+			rigidBody2D.AddForce(Vector2.right*speed);
+			transform.rotation=Quaternion.Euler(new Vector3(0,0,0));
 		}
 
 		if (Input.GetKey(KeyCode.LeftArrow)){
-			transform.Translate(-Vector2.right*speed);
+			rigidBody2D.AddForce(-Vector2.right*speed);
+			transform.rotation=Quaternion.Euler(new Vector3(0,0,180));
 		}
 
 		if (Input.GetKey(KeyCode.UpArrow)){
-			transform.Translate(Vector2.up*speed);
+			rigidBody2D.AddForce(Vector2.up*speed);
+			transform.rotation=Quaternion.Euler(new Vector3(0,0,90));
 		}
 
 		if (Input.GetKey(KeyCode.DownArrow)){
-			transform.Translate(-Vector2.up*speed);
+			rigidBody2D.AddForce(-Vector2.up*speed);
+			transform.rotation=Quaternion.Euler(new Vector3(0,0,270));
 		}
+		s = rigidBody2D.velocity.magnitude;
 	}
 }
